1ecd25eb8e99456aebfc3b33cea92a070e004680
rofviv <royvillarroel94@gmail.com>
2025-04-11 16:44:36 -0400

    lidar test

--- src/scripts/testing/lidar.py
+++ src/scripts/testing/lidar.py
@@ -1,14 +1,13 @@
 from rplidar import RPLidar
 import time
 
-# Set Correct Com/USB port  Windows :  COM1,COM3,COM3, ... or Raspberry Pi Linux :  /dev/ttyUSB0, /dev/ttyUSB1, ....
-lidar = RPLidar('/dev/rplidar', 256000)
+lidar = RPLidar('/dev/ttyUSB0', 256000)
 
 range_distance = 500
-range_angule = 50
-half_range_angule = range_angule / 2
-min_angle = 0.0
-max_angle = 359.9
+# range_angule = 50
+# half_range_angule = range_angule / 2
+# min_angle = 0.0
+# max_angle = 359.9
 
 info = lidar.get_info()
 print(info)
@@ -21,8 +20,9 @@ print(f"Range distance = {range_distance}")
 try:
     for scan in lidar.iter_scans():
         for (_, angle, distance) in scan:
+            if distance < range_distance:
             # Verifica si el ángulo está en el rango de 350 a 359 grados o de 0 a 20 grados
-            if (angle >= (max_angle - half_range_angule) or angle <= (min_angle + half_range_angule)) and distance < range_distance:
+            # if (angle >= (max_angle - half_range_angule) or angle <= (min_angle + half_range_angule)) and distance < range_distance:
                 print(f"Objeto detectado a {distance} mm en el ángulo {angle} grados")
                 print("\n")
 
